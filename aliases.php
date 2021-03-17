2634bef626a8f612345081ea6198b17ec88d70c9
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-17 10:35:38 -0400

    use new lazy Class Loader aliasing

--- aliases.php
+++ aliases.php
@@ -1,4 +1,7 @@
 <?php
+
+use Winter\Storm\Support\ClassLoader;
+
 /**
  * To allow compatibility with plugins that extend the original RainLab.Builder plugin, this will alias those classes to
  * use the new Winter.Builder classes.
@@ -62,8 +65,4 @@ $aliases = [
     Winter\Builder\Widgets\DefaultBehaviorDesignTimeProvider::class => 'RainLab\Builder\Widgets\DefaultBehaviorDesignTimeProvider',
 ];
 
-foreach ($aliases as $original => $alias) {
-    if (!class_exists($alias)) {
-        class_alias($original, $alias);
-    }
-}
+app(ClassLoader::class)->addAliases($aliases);
