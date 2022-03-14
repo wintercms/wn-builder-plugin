1752a9c1bd6fbae80d1911e944dbbcb95c9f0999
Ben Thomson <git@alfreido.com>
2022-03-14 10:40:55 +0800

    Allow $jsonable to be filled into model automatically (#28)
    Co-authored-by: Luke Towers <github@luketowers.ca>

--- tests/unit/classes/ModelFileParserTest.php
+++ tests/unit/classes/ModelFileParserTest.php
@@ -7,26 +7,58 @@ use Winter\Builder\Classes\ModelFileParser;
  */
 class ModelFileParserTest extends \TestCase
 {
-    /**
-     * ModelFileParser instance
-     *
-     * @var ModelFileParser
-     */
-    protected $modelFileParser;
-
-    public function setUp(): void
-    {
-        $this->modelFileParser = new ModelFileParser();
-    }
-
     public function testExtractModelInfoFromSource()
     {
-        $modelInfo = $this->modelFileParser->extractModelInfoFromSource(
-            file_get_contents(__DIR__ . '/../../fixtures/pluginfixture/models/SimpleModel.php')
-        );
+        $parser = new ModelFileParser(__DIR__ . '/../../fixtures/pluginfixture/models/SimpleModel.php');
+        $modelInfo = $parser->extractModelInfoFromSource();
 
         $this->assertEquals('Winter\Builder\Tests\Fixtures\PluginFixture\Models', $modelInfo['namespace']);
         $this->assertEquals('SimpleModel', $modelInfo['class']);
         $this->assertEquals('plugin_fixture_simple_model', $modelInfo['table']);
+    }
+
+    public function testGetSource()
+    {
+        $parser = new ModelFileParser(__DIR__ . '/../../fixtures/pluginfixture/models/SimpleModel.php');
+        $source = $parser->getSource();
+
+        $this->assertEquals(file_get_contents(__DIR__ . '/../../fixtures/pluginfixture/models/SimpleModel.php'), $source);
+    }
+
+    public function testGetJsonable()
+    {
+        $parser = new ModelFileParser(__DIR__ . '/../../fixtures/pluginfixture/models/SimpleModel.php');
+        $jsonable = $parser->getJsonable();
+        $this->assertNull($jsonable);
+
+        $parser = new ModelFileParser(__DIR__ . '/../../fixtures/pluginfixture/models/ArrayDataModel.php');
+        $jsonable = $parser->getJsonable();
+        $this->assertEquals([
+            'data',
+        ], $jsonable);
+    }
+
+    public function testSetJsonable()
+    {
+        $parser = new ModelFileParser(__DIR__ . '/../../fixtures/pluginfixture/models/ArrayDataModel.php');
+        $parser->setJsonable([
+            'field_1',
+            'field_2',
+        ]);
+        $source = $parser->getSource();
+
+        $this->assertStringContainsString('public $jsonable = [\'field_1\', \'field_2\'];', $source);
+    }
+
+    public function testSetJsonableAddProperty()
+    {
+        $parser = new ModelFileParser(__DIR__ . '/../../fixtures/pluginfixture/models/SimpleModel.php');
+        $parser->setJsonable([
+            'field_1',
+            'field_2',
+        ]);
+        $source = $parser->getSource();
+
+        $this->assertStringContainsString('public $jsonable = [\'field_1\', \'field_2\'];', $source);
     }
 }
